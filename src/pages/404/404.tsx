7e4ca1b6962a8467a747b1d0401cdb5481f76f1d
AFCMS <afcm.contact@gmail.com>
2025-02-03 12:11:04 +0100

    Home page button instead of redirect on 404

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,9 +1,21 @@
-import { useNavigate } from "react-router";
+import { NavLink } from "react-router";
 
 export default function Unknown404() {
-  const navigate = useNavigate();
-
-  setTimeout(() => navigate("/"), 3000);
-
-  return <span>404</span>;
+  return (
+    <div className="hero bg-base-200 sm:mt-24 p-6">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src="https://www.superherodb.com/pictures2/portraits/10/100/791.jpg"
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div className="">
+          <h1 className="text-5xl font-bold">Uh? This page doesn't exist</h1>
+          <p className="py-6">Go back to the homa page.</p>
+          <NavLink to="/" className="btn btn-primary">
+            Home
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 }
